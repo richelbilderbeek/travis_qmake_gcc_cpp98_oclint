1869985d5d5a8bebf8a686f4bdaa219727f622ce
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-25 16:35:20 +0200

    Made script more robust

--- do_oclint.sh
+++ do_oclint.sh
@@ -2,9 +2,12 @@
 
 # Clean main should work
 ./oclint-0.10.3/bin/oclint -o log_correct.txt -max-priority-1 0 -max-priority-2 0 -max-priority-3 0 main_correct.cpp -- -c > /dev/null
-num_lines=`wc -l log_correct.txt | cut -d " " -f 1`
 
-if [ $num_lines -eq 8 ]; 
+# Will be 1 if success
+# Will be 0 if fail
+success=`egrep "FilesWithViolations=0 P1=0 P2=0 P3=0" log_correct.txt | wc -l`
+
+if [ $success -eq 1 ]; 
 then
   echo "Clean code indeed"
 else
@@ -14,9 +17,12 @@ fi
 
 # Dirty code should be detected
 ./oclint-0.10.3/bin/oclint -o log_incorrect.txt -max-priority-1 0 -max-priority-2 0 -max-priority-3 0 main_incorrect.cpp -- -c > /dev/null
-num_lines=`wc -l log_incorrect.txt | cut -d " " -f 1`
 
-if [ $num_lines -eq 8 ]; 
+# Will be 1 if success
+# Will be 0 if fail
+success=`egrep "FilesWithViolations=0 P1=0 P2=0 P3=0" log_incorrect.txt | wc -l`
+
+if [ $success -eq 1 ]; 
 then
   echo "Incorrectly labeled dirty code correct"
   exit 1
